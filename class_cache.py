0d2c3dbfef7bd487dab2149a4a77c0e89a79bbee
Per <per@starke-team.de>
2014-03-30 15:34:59 +0200

    Sonntag-Sofa

---
+++ class_cache.py
@@ -0,0 +1,20 @@
+__author__ = 'Per'
+# 25.12.13
+# works, extendable
+
+class cache(object):
+    def __init__(self, gccode, gcname):
+        self.Gccode = gccode
+        self.Gcname = gcname
+
+    def printcache(self):
+        print("Gccode       :", self.Gccode)
+        print("Name         :", self.Gcname)
+
+
+        def gccode(self):
+        return(self.Gccode)
+    def gcname(self):
+        return(self.Gcname)
+
+
