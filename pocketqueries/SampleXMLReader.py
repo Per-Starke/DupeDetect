9607739c940e30fa450a2839815a1b0b5f3cc8dd
Dr. Gernot Starke <gs@gernotstarke.de>
2014-04-12 23:27:50 +0200

    process file, waypoints and logs

--- pocketqueries/SampleXMLReader.py
+++ pocketqueries/SampleXMLReader.py
@@ -5,21 +5,32 @@ import xml.dom.minidom
 
 filename = 'test-cachelist.xml'
 
-def printWayPoint( wpt, nrOfLogs ):
-	wname = wpt.getElementsByTagName('name')[0].firstChild.wholeText
-	print "Waypoint %s contains %d logs" % (wname, nrOfLogs)
- 	print '    desc: ', wpt.getElementsByTagName('desc')[0].firstChild.wholeText
- 	print "    lat: %s, lon: %s " % (wpt.attributes['lat'].value, wpt.attributes['lon'].value)
- 	print '-'*10, "\n"
-
 def printFileStatistics( dom, waypoints ):
 	nrOfLogs = dom.getElementsByTagName('groundspeak:log').length
 	nrOfCaches = dom.getElementsByTagName('groundspeak:cache').length
-
 	print "\n"*3
 	print "File Statistics:"
 	print 'file %s contains %d waypoints, %d caches and %d logs.' % (filename, waypoints.length, nrOfCaches, nrOfLogs)
 	print '='*70, "\n"
+
+
+
+def printWayPoint( wpt, nrOfLogs ):
+	wname = wpt.getElementsByTagName('name')[0].firstChild.wholeText
+	print '-'*20
+	print "Waypoint %s contains %d logs" % (wname, nrOfLogs)
+ 	print '    desc: ', wpt.getElementsByTagName('desc')[0].firstChild.wholeText
+ 	print "    lat: %s, lon: %s " % (wpt.attributes['lat'].value, wpt.attributes['lon'].value)
+ 	
+
+def printLogEntry( logEntry ):
+	logId = logEntry.attributes['id'].value
+	logDate = logEntry.getElementsByTagName('groundspeak:date')[0].firstChild.wholeText
+	logType = logEntry.getElementsByTagName('groundspeak:type')[0].firstChild.wholeText
+	finder  = logEntry.getElementsByTagName('groundspeak:finder')[0].firstChild.wholeText
+	print '     ', '- '*5
+	print ' '*8, 'id = %s, %s, logdate = %s by %s' % (logId, logType, logDate, finder)
+
 
 
 
@@ -32,5 +43,7 @@ printFileStatistics(dom, waypoints)
 for wpt in waypoints:
 	logs = wpt.getElementsByTagName('groundspeak:log')
 	printWayPoint( wpt, logs.length )
+	for log in logs:
+		printLogEntry( log )
     
    
