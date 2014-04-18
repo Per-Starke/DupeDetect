a46afbda0e7eac6d6b2cb09efe668478583a039a
Per Starke <per@starke-team.de>
2014-04-18 08:50:31 +0100

    Cleaned up some code

--- class_logentry.py
+++ class_logentry.py
@@ -5,28 +5,28 @@ __author__ = 'Per'
 
 class logentry(object):
     def __init__(self, logid, logtype, logdate, logtxt):
-        self.Logid = logid
-        self.Logtype = logtype
-        self.Logdate = logdate
-        self.Logtxt = logtxt
+        self.logid = logid
+        self.logtype = logtype
+        self.logdate = logdate
+        self.logtxt = logtxt
 
 
 
     def printlog(self):
-        return(self.Logid, "logtype      :", self.Logtype, "logdate      :", self.Logdate, "logtext      :", self.Logtxt[0:50] + "...")
+        return(self.logid, "logtype      :", self.logtype, "logdate      :", self.logdate, "logtext      :", self.logtxt[0:50] + "...")
         # print("log-ID       :", self.Logid)
         # print("logtype      :", self.Logtype)
         # print("logdate      :", self.Logdate)
         # print("logtext      :", self.Logtxt)
 
-    def logtype(self):
-        return(self.Logtype)
-
-    def logdate(self):
-        return(self.Logdate)
-
-    def logtxt(self):
-        return(self.Logtxt)
-
-    def logid(self):
-        return(self.Logid)
+    # def logtype(self):
+    #     return(self.Logtype)
+    #
+    # def logdate(self):
+    #     return(self.Logdate)
+    #
+    # def logtxt(self):
+    #     return(self.Logtxt)
+    #
+    # def logid(self):
+    #     return(self.Logid)
